7aff28717f040b91ee62429b0c3861610beeef3f
Moro-Ryu <moro.wiedzmin@gmail.com>
2021-11-28 18:40:43 +0200

    Added module "UnitTest" and maked a test of the "Sinus" class

---
+++ src/Test/SinusTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.junit.Test;
+
+public class SinusTest {
+
+    @Test
+    public void operation() {
+
+        double inputNumber = 5.67;
+
+        System.out.println(Math.sin(inputNumber));
+    }
+}
+
